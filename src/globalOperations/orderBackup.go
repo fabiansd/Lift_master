2dc1e808679e8c8370292beb9dd51393af9c18f7
vetleab <vetleab@stud.ntnu.no>
2016-04-17 16:36:34 +0200

    Update orderBackup.go

--- src/globalOperations/orderBackup.go
+++ src/globalOperations/orderBackup.go
@@ -6,8 +6,7 @@ import (
 	"time"
 )
 
-//Nye channels
-var backupRequest [4][3]bool //Alle bestillinger som blir assigned blir registrert her som true
+var backupRequest [4][3]bool 
 var backupChan = make(chan Order, 100)
 var timeoutChan = make(chan Order, 100)
 
@@ -16,7 +15,7 @@ func BackupHandler(newOrderChan chan elevatorOperations.Keypress, orderCompleteC
 	for {
 		select {
 		case nO := <-backupChan:
-			if backupRequest[nO.Floor][nO.Button] == false { // "" = false
+			if backupRequest[nO.Floor][nO.Button] == false { 
 				backupRequest[nO.Floor][nO.Button] = true
 				elevatorOperations.Fsm_printrequest(backupRequest, "The backup requests")
 				go completionTimer(nO)
